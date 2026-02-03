ebb3653b00e818df101c4390392138dd3916ec42
Pavan Bairagi <pavanb.bca2023@ssism.org>
2026-02-03 18:59:28 +0530

    Delete backend_core directory

--- backend_core/parking/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ParkingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'backend_core.parking'
-    verbose_name = 'Parking Management'
